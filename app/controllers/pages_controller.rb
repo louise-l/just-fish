470da635c272d6924af0c67dcf872ba206f5a001
louise longle <louise.longle@gmail.com>
2021-05-25 13:28:01 +0200

    created page product category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,28 @@ class PagesController < ApplicationController
     @galleries = Gallery.all
   end
 
+  def products
+    @poissons_frais = Gallery.find_by(name: "poissons-frais").images.last.key
+    @huitres = Gallery.find_by(name: "huitres").images.last.key
+    @coquillages = Gallery.find_by(name: "coquillages").images.last.key
+    @poissons_fumes = Gallery.find_by(name: "poissons-fumes").images.last.key
+    @produits_conditionnes = Gallery.find_by(name: "produits-contionnés").images.last.key
+    @produits_surgeles = Gallery.find_by(name: "produits-surgeles").images.last.key
+    @epicerie = Gallery.find_by(name: "epicerie").images.last.key
+    @alcools = Gallery.find_by(name: "alcools").images.last.key
+
+    @poissons_frais_txt = Text.find_by(name: 'poissons-frais')
+    @huitres_txt = Text.find_by(name: 'huitres')
+    @coquillages_txt = Text.find_by(name: 'coquillages-crustaces')
+    @poissons_fumes_txt = Text.find_by(name: 'poissons-fumes')
+    @produits_conditionnes_txt = Text.find_by(name: 'produits-conditionnes')
+    @produits_surgeles_txt = Text.find_by(name: 'produits-surgeles')
+    @epicerie_txt = Text.find_by(name: 'epicerie')
+    @alcools_txt = Text.find_by(name: 'alcools')
+
+
+  end
+
   def about
     @horaires = Text.find_by(name: 'horaires')
     @livraisons = Text.find_by(name: 'livraison')
